4d3e30844c922ea17872a1ef3a82b3c69736d091
Ahmad Hayyan <ahmadhayyan9@gmail.com>
2021-04-27 11:06:20 +0700

    Update mainJS.js

--- res/mainJS.js
+++ res/mainJS.js
@@ -20,18 +20,30 @@ function closeMenu(){
 	document.getElementById('copyright').style.display='block';
 	
 	var home = document.getElementById("Home");
-    if(home != null){
+    if(home){
         home.style.display='block';
+		document.getElementById('mobileMenuFull').style.display='none';
     }
 	
-	document.getElementById('mobileMenuFull').style.display='none';
 	document.body.style.overflowY='auto';
 }
 
 var image;
-function openImage(id){
-	image = id;
-	document.getElementById(id).classList.add('imgClick');
+function openImage(id, imageURL){
+	if (id != ""){
+		image = id;
+		document.getElementById(id).classList.add('imgClick');
+	}
+	if (imageURL != undefined){
+		var html = document.createElement("div");
+		html.id = "openedImg";
+		html.className = "imgPost imgClick";
+		html.style.cssText  = "background-image: url("+imageURL+");";
+		document.body.appendChild(html);
+		
+		image = "openedImg";
+	}
+	
 	document.getElementById('coverImg').style.display = 'block';
 	document.getElementById('closeImg').style.display = 'block';
 	document.body.style.overflow = 'hidden';
@@ -41,6 +53,11 @@ function closeImage(){
 	document.getElementById('closeImg').style.display='none';
 	document.getElementById('coverImg').style.display='none';
 	document.body.style.overflow = 'auto';
+	
+	var html = document.getElementById("openedImg");
+	if (html){
+		html.parentNode.removeChild(html);
+	}
 }
 
 window.addEventListener("resize", function(event) {
